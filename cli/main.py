4236dfa27d6c9e65ee3073ed205150be5fd73c20
JoshYuJump <josh.yu_8@live.com>
2022-10-11 17:43:02 +0800

    Added Bali App `run` command and `shell` command

--- cli/main.py
+++ cli/main.py
@@ -3,13 +3,14 @@ import os
 import typer
 
 from cli import biz, __version__
+from . import run
 
 app = typer.Typer()
 
 
 @app.command()
 def add(service: str, target_dir: str = ''):
-    """add input_service_name's proto_file to current_dir/service_name"""
+    """Add input_service_name's proto_file to current_dir/service_name"""
     proto_repo = 'proto'
 
     typer.echo(f'{os.linesep} 🏝 bali-cli v{__version__}')
@@ -20,8 +21,17 @@ def add(service: str, target_dir: str = ''):
 
 @app.command()
 def build():
-    """compile proto_file under cwd/services/rpc"""
+    """Compile proto_file under cwd/services/rpc"""
     typer.echo(f'{os.linesep} 🏝 bali-cli v{__version__}')
     typer.echo(f'{os.linesep} 🚀 Start build ...')
     biz.build_service()
     typer.echo(f'{os.linesep} ✅ Service build successfully !')
+
+
+@app.command()
+def shell():
+    """Enter Bali App shell"""
+    os.system("python3 main.py launch --shell")
+
+
+app.add_typer(run.app, name="run")
